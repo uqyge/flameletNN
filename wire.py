06c5a81e74e4cfdb3bb505d884f1b71e7e469e77
eg <y.ge1222@gmail.com>
2020-07-28 15:18:12 +0200

    post plots

--- wire.py
+++ wire.py
@@ -1,12 +1,18 @@
 #%%
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
+import scipy.signal as ss
 from plotly.subplots import make_subplots
 
 from utils.data_reader import read_h5_data
 
 input_features = ["f", "zeta", "pv"]
+
+with open("GRI_species_order_lu13", "r") as f:
+    labels13 = [a.rstrip() for a in f.readlines()]
 
 with open("GRI_species_order", "r") as f:
     labels = [a.rstrip() for a in f.readlines()]
@@ -14,34 +20,29 @@ labels.append("T")
 labels.append("PVs")
 
 x, y, df, in_scaler, out_scaler = read_h5_data(
-    "./data/tables_of_fgm.h5",
+    # "./data/tables_of_fgm.h5",
+    "./data/tables_of_fgm.parquet",
     input_features=input_features,
     labels=labels,
     i_scaler="no",
     o_scaler="cbrt_std",
 )
 df["zetaLevel"] = df.zeta.apply(str)
-
+df_filtered = df.copy()
 
 #%%
-def wireframe_html(sp, filtered=False):
-
+def wireframe_html(df, df_filtered, sp, filtered=False):
+    df = df
+    df_filtered = df_filtered
     wireframe = []
 
-    for zetaLevel in list(set(df.zetaLevel)):
-        df_z0 = df[df.zetaLevel == zetaLevel]
-
-        x_grid = df_z0.f.values.reshape(501, 501)
-        y_grid = df_z0.pv.values.reshape(501, 501)
-        z_grid = df_z0[sp].values.reshape(501, 501)
-        if filtered == True:
-            z_grid = ss.savgol_filter(z_grid, 51, 4)
-
-        #%%
+    def plot_line(x_grid, y_grid, z_grid, wireName, color):
         lines = []
-        line_marker = dict(color="#0066FF", width=2)
-        # line_marker = dict(width=2)
+        line_marker = dict(color=color, width=2)
         legendFlag = True
+
+        # wireName = f"{sp}:z={zetaLevel}(filtered:{filtered})"
+
         for i, j, k in zip(x_grid, y_grid, z_grid):
             lines.append(
                 go.Scatter3d(
@@ -50,8 +51,8 @@ def wireframe_html(sp, filtered=False):
                     z=k,
                     mode="lines",
                     line=line_marker,
-                    legendgroup=f"{sp}:z={zetaLevel}(filtered:{filtered})",
-                    name=f"{sp}:z={zetaLevel}(filtered:{filtered})",
+                    legendgroup=wireName,
+                    name=wireName,
                     showlegend=legendFlag,
                 )
             )
@@ -65,67 +66,144 @@ def wireframe_html(sp, filtered=False):
                     mode="lines",
                     line=line_marker,
                     # legendgroup=f"{sp}:z={zetaLevel}",
-                    legendgroup=f"{sp}:z={zetaLevel}(filtered:{filtered})",
+                    legendgroup=wireName,
                     showlegend=False,
                 )
             )
+
         wireframe.append(lines)
 
-    #%%
-    fig = go.Figure(data=[line for lines in wireframe for line in lines[::10]])
-    fig.show()
-    #%%
+    for zetaLevel in list(set(df.zetaLevel)):
+        df_z0 = df[df.zetaLevel == zetaLevel]
 
-    # if filtered == True:
-    #     fig.write_html(f"./wireframe/{sp}_wire_filtered.html")
-    # else:
-    #     fig.write_html(f"./wireframe/{sp}_wire.html")
+        x_grid = df_z0.f.values.reshape(501, 501)
+        y_grid = df_z0.pv.values.reshape(501, 501)
+        z_grid = df_z0[sp].values.reshape(501, 501)
+        # if (filtered == True) and zetaLevel not in ["0.0"]:
+        if (filtered == True):
+            if zetaLevel in ["0.0","0.11"]:
+                z_grid_filtered = ss.savgol_filter(z_grid, 7, 4)
+            else:
+                z_grid_filtered = ss.savgol_filter(z_grid, 31, 4)
+            ## update filtered values
+            df_filtered.loc[
+                df_filtered.zetaLevel == zetaLevel, sp
+            ] = z_grid_filtered.reshape(-1, 1)
+            wireName = f"{sp}:z={zetaLevel}(filtered:{filtered})"
+            plot_line(x_grid, y_grid, z_grid_filtered, wireName, color="#0066ff")
+
+        wireName = f"{sp}:z={zetaLevel}"
+        plot_line(x_grid, y_grid, z_grid, wireName, color="#ff0000")
+
+    fig = go.Figure(data=[line for lines in wireframe for line in lines[::10]])
+    # fig.show()
+    if filtered == True:
+        fig.write_html(f"./wireframe/{sp}_wire_filtered.html")
+    else:
+        fig.write_html(f"./wireframe/{sp}_wire.html")
 
 
 #%%
-# for sp in labels:
-#     wireframe_html(sp)
+for i in range(0,3):
+    print("i = ",i)
+    df = df_filtered.copy()
+    # labelList = labels13+['T','PVs']
+    labelList = labels
+    # labelList = ['O','PVs','HO2','CH3']
+    for sp in enumerate(labelList):
+        # wireframe_html(sp)
+        print(f"{sp[0]+1}/{len(labelList)},{sp[1]}")
+        wireframe_html(df=df, df_filtered=df_filtered, sp=sp[1], filtered=True)
 
+
+    df_filtered.drop("zetaLevel",axis=1,inplace=True)
+    df_filtered = df_filtered.clip(lower=0)
+
+
+    # df_filtered["zetaLevel"]=df["zetaLevel"]
+    df_filtered["zetaLevel"] = df_filtered.zeta.apply(str)
+    df["zetaLevel"] = df.zeta.apply(str)
+# df = df_filtered
+# %%
+df_filtered.to_parquet("df_filtered.parquet")
 
 # %%
-import matplotlib.pyplot as plt
+f_id = -1
+zetaLevel = "0.44"
+# for sp in labels13+['PVs']:
+for sp in [ "PVs", "O",'HO2','CH3']:
+# for sp in ["PVs", "CH2O","CH3","O"]:
+    val_org = df[df.zetaLevel == zetaLevel][sp].values.reshape(501, 501)
+    # val_filtered = ss.savgol_filter(val_org, 31, 4)
+    val_filtered = df_filtered[df_filtered.zetaLevel == zetaLevel][sp].values.reshape(
+        501, 501
+    )
 
-import scipy.signal as ss
-
-# %%
-df_tmp = df[df.zetaLevel == "0.33"]
-df_filtered = df_tmp.copy()
-# %%
-# sp = "PVs"
-# for sp in labels:
-for sp in ["CH3O", "PVs", "T"]:
-    val_org = df_tmp[sp].values.reshape(501, 501)
-    val_filtered = ss.savgol_filter(val_org, 51, 4)
-
-    df_filtered[sp] = val_filtered.reshape(-1, 1)
-
-    f_id = -20
     plt.figure()
-    plt.plot(val_org[f_id], "r:")
+    plt.plot(val_org[f_id], "r-")
     plt.plot(val_filtered[f_id])
     plt.title(sp)
     plt.show()
 
+# %%
 
 # %%
-wireframe_html("PVs")
+%%time
+# a = pd.read_hdf('df_test.h5')
+dataFile='./data/tables_of_fgm.h5'
+a = pd.read_hdf(dataFile)
+# %%
+a.to_parquet('tables_of_fgm.parquet')
+# a.to_hdf('df_test.h5',key='hdf')
+# %%
+%%time
+b = pd.read_parquet('tables_of_fgm.parquet')
+# b = pd.read_hdf('df_test.h5')
+b.shape
+# %%
+dataFile.split('.')[-1]=='h5'
 
 # %%
-wireframe_html("PVs", filtered=True)
+df_filtered.shape
+
+# %%
+df_filtered.drop("zetaLevel",axis=1,inplace=True)
+df_filtered = df_filtered.clip(lower=0)
+df_filtered.to_parquet("df_filtered.parquet")
+# %%
+df_filtered = pd.read_parquet('df_filtered.parquet')
+df_filtered['zetaLevel'] = df['zetaLevel']
 
 
 #%%
-# %%
+import tensorflow as tf
+import pickle
+
+
+with open('out_scaler.pkl','rb') as f:
+    out_scaler = pickle.load(f)
+
+model = tf.keras.models.load_model('wudi_4x100.h5')
 
 # %%
-px.scatter_3d(data_frame=df_tmp, x="f", y="pv", z=sp)
+x = df[input_features].values
+pred_org=model.predict(x,batch_size=1024)
 
 # %%
-px.scatter_3d(data_frame=df_filtered, x="f", y="pv", z=sp)
+out_sps = labels13+['T','PVs']
+out_sps.remove('N2')
+
+
+# %%
+df_pred=pd.DataFrame(out_scaler.inverse_transform(pred_org),columns=out_sps)
+
+df_model=pd.concat([df[input_features],df_pred],axis=1)
+df_model['zetaLevel']=df_model.zeta.apply(str)
+# %%
+
+
+# %%
+px.scatter_3d(data_frame=df_model[df_model.zetaLevel=="0.0"].sample(n=10_000),x='f',y='pv',z='PVs'
+)
 
 # %%
---
+++ wire_net.py
@@ -0,0 +1,306 @@
+#%%
+import os
+
+# %%
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# %%
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.models import Model, load_model
+
+from utils.customObjects import SGDRScheduler, coeff_r2
+from utils.data_reader import read_h5_data
+from utils.resBlock import res_block, res_block_org
+from utils.writeANNProperties import writeANNProperties
+
+##########################
+# Parameters
+n_neuron = 100
+branches = 3
+scale = 3
+batch_size = 256
+epochs = 2000
+vsplit = 0.1
+batch_norm = False
+
+# define the type of scaler: MinMax or Standard
+scaler = "Standard"  # 'Standard' 'MinMax'
+
+##########################
+
+labels = []
+
+with open("GRI_species_order_lu13", "r") as f:
+    labels = [a.rstrip() for a in f.readlines()]
+
+# append other fields: heatrelease,  T, PVs
+# labels.append('heatRelease')
+labels.append("T")
+labels.append("PVs")
+labels.remove("N2")
+
+# # tabulate psi, mu, alpha
+# labels.append('psi')
+# labels.append('mu')
+# labels.append('alpha')
+
+
+# DO NOT CHANGE THIS ORDER!!
+input_features = ["f", "zeta", "pv"]
+
+
+# read in the data
+X, y, df, in_scaler, out_scaler = read_h5_data(
+    # "./data/tables_of_fgm.h5",
+    "./data/df_filtered_3.parquet",
+    input_features=input_features,
+    labels=labels,
+    i_scaler="no",
+    o_scaler="cbrt_std",
+)
+# ('./data/tables_of_fgm.h5',key='of_tables',
+# in_labels=input_features, labels = labels,scaler=scaler)
+
+# split into train and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01)
+
+# %%
+print("set up ANN")
+
+# ANN parameters
+dim_input = X_train.shape[1]
+dim_label = y_train.shape[1]
+
+
+# This returns a tensor
+inputs = Input(shape=(dim_input,))  # ,name='input_1')
+
+# a layer instance is callable on a tensor, and returns a tensor
+x = Dense(n_neuron, activation="relu")(inputs)
+
+x = res_block_org(x, n_neuron, stage=1, block="a", bn=batch_norm)
+x = res_block_org(x, n_neuron, stage=1, block="b", bn=batch_norm)
+x = res_block_org(x, n_neuron, stage=1, block="c", bn=batch_norm)
+x = res_block_org(x, n_neuron, stage=1, block="d", bn=batch_norm)
+
+x = Dense(100, activation="relu")(x)
+
+predictions = Dense(dim_label, activation="linear")(x)
+
+model = Model(inputs=inputs, outputs=predictions)
+
+learning_rate = 1e-3
+# global_step = 1000
+first_decay_steps = 137
+# first_decay_steps = np.ceil(epoch_size / batch_size)
+# lr_decayed = tf.keras.experimental.cosine_decay_restarts(learning_rate, global_step,
+#                                    first_decay_steps)
+lr_decayed = tf.keras.experimental.CosineDecayRestarts(
+    learning_rate, first_decay_steps, t_mul=2
+)
+
+
+model.compile(
+    loss="mse",
+    optimizer=tf.keras.optimizers.Adam(learning_rate=lr_decayed),
+    # optimizer=tf.keras.optimizers.RMSprop(),
+    # optimizer=tf.keras.optimizers.SGD(learning_rate=lr_decayed, momentum=0.9),
+    metrics=["accuracy"],
+)
+# get the model summary
+model.summary()
+
+# WARM RESTART
+batch_size_list = [
+    # batch_size,
+    batch_size * 64,
+    # batch_size * 16,
+    # batch_size * 4,
+    # batch_size
+]
+
+for this_batch in batch_size_list:
+    # checkpoint (save the best model based validate loss)
+    filepath = "./tmp/weights.best.cntk.hdf5"
+
+    # check if there are weights
+    if os.path.isfile(filepath):
+        print("read model")
+        model.load_weights(filepath)
+
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor="val_loss",
+        verbose=1,
+        save_best_only=True,
+        mode="min",
+        period=10,
+    )
+    epoch_size = X_train.shape[0]
+    a = 0
+    base = 2
+    clc = 2
+    for i in range(6):
+        a += base * clc ** (i)
+    print(a)
+    epochs, c_len = a, base
+    schedule = SGDRScheduler(
+        min_lr=1e-5,
+        max_lr=1e-3,
+        steps_per_epoch=np.ceil(epoch_size / batch_size),
+        cycle_length=c_len,
+        lr_decay=0.8,
+        mult_factor=clc,
+    )
+
+    class lr_log(tf.keras.callbacks.Callback):
+        # def on_train_batch_end(self, batch, log=None):
+        # print("wudi")
+
+        def on_epoch_end(self, batch, log={}):
+            log.update({"lr": self.model.optimizer._decayed_lr("float32").numpy()})
+            print(
+                "lr_decay:",
+                self.model.optimizer._decayed_lr("float32").numpy(),
+                "lr:",
+                # self.model.optimizer.lr(),
+            )
+
+    # callbacks_list = [checkpoint, schedule, lr_log()]
+    callbacks_list = [checkpoint, lr_log()]
+
+    # fit the model
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=epochs,
+        batch_size=this_batch,
+        validation_split=vsplit,
+        verbose=2,
+        callbacks=callbacks_list,
+        shuffle=True,
+    )
+
+# plt.plot(history.history["lr"])
+#%%
+
+
+# # %%
+predict_val = model.predict(X_test)
+
+X_test_df = pd.DataFrame(in_scaler.inverse_transform(X_test), columns=input_features)
+y_test_df = pd.DataFrame(out_scaler.inverse_transform(y_test), columns=labels)
+
+sp = "PVs"
+
+# loss
+fig = plt.figure()
+plt.semilogy(history.history["loss"])
+if vsplit:
+    plt.semilogy(history.history["val_loss"])
+plt.title("mse")
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.legend(["train", "test"], loc="upper right")
+plt.savefig("./exported/Loss_%s_%s_%i.eps" % (sp, scaler, n_neuron), format="eps")
+plt.show(block=False)
+
+predict_df = pd.DataFrame(out_scaler.inverse_transform(predict_val), columns=labels)
+
+plt.figure()
+plt.title("Error of %s " % sp)
+plt.plot((y_test_df[sp] - predict_df[sp]) / y_test_df[sp])
+plt.title(sp)
+plt.savefig("./exported/Error_%s_%s_%i.eps" % (sp, scaler, n_neuron), format="eps")
+plt.show(block=False)
+
+plt.figure()
+plt.scatter(predict_df[sp], y_test_df[sp], s=1)
+plt.title("R2 for " + sp)
+plt.savefig("./exported/R2_%s_%s_%i.eps" % (sp, scaler, n_neuron), format="eps")
+plt.show(block=False)
+
+for sp in labels:
+    print(sp, r2_score(predict_df[sp], y_test_df[sp]))
+
+#%%
+a = (y_test_df[sp] - predict_df[sp]) / y_test_df[sp]
+test_data = pd.concat([X_test_df, y_test_df], axis=1)
+pred_data = pd.concat([X_test_df, predict_df], axis=1)
+
+test_data.to_hdf("sim_check.H5", key="test")
+pred_data.to_hdf("sim_check.H5", key="pred")
+
+# Save model
+sess = K.get_session()
+saver = tf.train.Saver(tf.global_variables())
+saver.save(sess, "./exported/my_model")
+model.save("FPV_ANN_tabulated_%s.H5" % scaler)
+
+# write the OpenFOAM ANNProperties file
+writeANNProperties(in_scaler, out_scaler, scaler)
+
+# Convert the model to
+# run -i k2tf.py --input_model='FPV_ANN_tabulated_Standard.H5' --output_model='exported/FPV_ANN_tabulated_Standard.pb'
+
+#%%
+model.save("wudi_4x100.h5")
+
+# %%
+
+# %%
+a = pd.read_parquet("./data/df_filtered.parquet")
+
+
+# %%
+a.drop("zetaLevel", axis=1, inplace=True)
+
+# %%
+a.to_parquet("./data/df_filtered_3.parquet")
+
+
+# %%
+with open("out_scaler.pkl", "wb") as f:
+    pickle.dump(out_scaler, f)
+
+# %%
+with open("out_scaler.pkl", "rb") as f:
+    a = pickle.load(f)
+
+
+# %%
+
+
+# %%
+a.inverse_transform(y)
+
+# %%
+import tensorflow as tf
+
+learning_rate = 1e-3
+global_step = 1000
+first_decay_steps = 1000
+# lr_decayed = tf.keras.experimental.cosine_decay_restarts(learning_rate, global_step,
+#                                    first_decay_steps)
+lr_decayed = tf.keras.experimental.CosineDecayRestarts(
+    learning_rate, global_step, first_decay_steps
+)
+
+# %%
+print(lr_decayed)
+
+# %%
+lr_decayed.numpy()
+# %%
+
+
+# %%
